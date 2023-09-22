d8da6f5016c84b7a7f2d5417b80ee1ec20a4aa7b
BrunaGPires <bruna.gpires@hotmail.com>
2023-09-22 17:08:37 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("(51)988156070", "NokiaTijolao", "880088", 64);
+Iphone iphone = new Iphone("(51)988156070", "IphoneSE", "880088", 128);
+
+Console.WriteLine("Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+Console.Write("\n");
+Console.WriteLine("Iphone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("X(Twitter)");
